0d53c94f5e791044cad9227dbe8d615e271ba4c0
huzaifak08 <hk7928042@gmail.com>
2024-02-15 21:40:03 +0500

    Factorial Allhamdullillah

---
+++ src/Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public void factorialMethod(){
+        System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int factorial = 1;
+
+        for(int i=1; i<=num; i++){
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial is: " + factorial);
+    }
+}
