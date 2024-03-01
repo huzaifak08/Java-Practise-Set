e65348c5f6c2c85d0f9f73e7677bae46a525976a
huzaifak08 <hk7928042@gmail.com>
2024-03-01 16:43:26 +0500

    Fibonacci Allhamdullillah

---
+++ src/FibonacciSeries.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+
+        System.out.print("Enter any number: "); // 5
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int first = 0;
+        int second = 1;
+
+        for (int i = 0; i <= n; ++i) {
+            System.out.print(first + ", "); // 0, 1, 1, 2, 3, 5,
+
+            int next = first + second;
+
+            first = second;
+            second = next;
+        }
+
+    }
+}
