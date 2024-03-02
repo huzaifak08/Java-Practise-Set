2a41d7af2a893330fadd3e83813e286a49e81a1c
huzaifak08 <hk7928042@gmail.com>
2024-03-02 20:52:20 +0500

    Prime number Allhamdullillah

---
+++ src/PrimeNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.print("Enter any number to check: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                System.out.println("Not Prime");
+                return;
+            } else {
+                System.out.println("Prime Number");
+            }
+        }
+    }
+}
