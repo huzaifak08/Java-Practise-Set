ab4dabbeb2cdd30279aed62d04ac2e6333902ec6
huzaifak08 <hk7928042@gmail.com>
2024-03-26 20:53:08 +0500

    Right angle triangle Allamdullillah

---
+++ src/PS_05_RightTriangle.java
@@ -0,0 +1,17 @@
+public class PS_05_RightTriangle {
+    public static void main(String[] args) {
+
+        //  *
+        //  **
+        //  ***
+        //  ****
+        //  *****
+
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
