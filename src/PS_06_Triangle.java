5292b0f716ba9dcc35764bb6257c35952655da9e
huzaifak08 <hk7928042@gmail.com>
2024-03-26 21:10:52 +0500

    Triangle Allamdullillah

---
+++ src/PS_06_Triangle.java
@@ -0,0 +1,30 @@
+public class PS_06_Triangle {
+    static void triangleStar(int n) {
+        // Number of Rows:
+        for (int i = 0; i < n; i++) {
+
+            // For Printing Spaces:
+            for (int j = n - i; j > 1; j--) {
+                System.out.print(" ");
+            }
+
+            // For Printing Stars:
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        //      *
+        //     * *
+        //    * * *
+        //   * * * *
+        //  * * * * *
+
+        triangleStar(5);
+    }
+}
