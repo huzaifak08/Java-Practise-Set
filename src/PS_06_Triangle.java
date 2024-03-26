67aae96bb6f353f96ebc8c8c7ac959092cac93f6
huzaifak08 <hk7928042@gmail.com>
2024-03-26 21:12:17 +0500

    Triangle Allamdullillah

--- src/PS_06_Triangle.java
+++ src/PS_06_Triangle.java
@@ -10,7 +10,7 @@ public class PS_06_Triangle {
 
             // For Printing Stars:
             for (int j = 0; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
 
