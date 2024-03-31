92d288da06f5dcdfc34e9bface0d5d44ec66f82b
huzaifak08 <hk7928042@gmail.com>
2024-03-31 15:56:20 +0500

    Reversed and Mirrored Right Triangls

---
+++ src/PS_14_MirroredRightTriangle.java
@@ -0,0 +1,20 @@
+public class PS_14_MirroredRightTriangle {
+    public static void main(String[] args) {
+
+        // *
+        // **
+        // ***
+        // ****
+        // *****
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = (5 - i); j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
